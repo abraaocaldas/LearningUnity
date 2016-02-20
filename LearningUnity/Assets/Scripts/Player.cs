bec62c9cbece34b397467dfcf4a66fcd033fccbf
Abraão Caldas <abraaocaldas@gmail.com>
2016-02-20 19:23:47 -0400

    Lesson #1

---
+++ LearningUnity/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+
+
+    }
+}

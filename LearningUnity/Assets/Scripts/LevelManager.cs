5124da9d10d20519e378b2426cf8bbdca64f6a81
Abraão Caldas <abraaocaldas@gmail.com>
2016-02-20 20:45:46 -0400

    Lesson #3

---
+++ LearningUnity/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckPoint;
+    private Player player;
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player Respawn");
+        player.transform.position = currentCheckPoint.transform.position;
+    }
+}

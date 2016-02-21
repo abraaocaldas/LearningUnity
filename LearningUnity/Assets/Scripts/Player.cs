9be2876f9cbfe41b1e31aaca09bed4b8d17b954d
Abraão Caldas <abraaocaldas@gmail.com>
2016-02-20 20:12:23 -0400

    Lesson #2
    Fixed KeyDown on Jump Event, Sorry!

--- LearningUnity/Assets/Scripts/Player.cs
+++ LearningUnity/Assets/Scripts/Player.cs
@@ -7,13 +7,37 @@ public class Player : MonoBehaviour {
     public float moveSpeed;
     public float jumpHeight;
 
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private bool doubleJumped = false;
+
 	// Use this for initialization
 	void Start () {
         GetComponent<Rigidbody2D>().freezeRotation = true;
 	}
 	
+    //Happens in 15 seconds interval
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
 	// Update is called once per frame
 	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            doubleJumped = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && !grounded && doubleJumped)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            doubleJumped = false;
+        }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -23,10 +47,7 @@ public class Player : MonoBehaviour {
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
-        }
+       
 
 
     }
